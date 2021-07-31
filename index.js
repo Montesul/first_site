6e4a2377659a2df7072ab81bae06762010270689
Montesul <lrz93@mail.ru>
2021-07-31 15:01:46 +0300

    add handlebars

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const express = require('express'),
-          app = express();
+app = express();
+
+const handlebars = require('express-handlebars')
+    .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
 app.use((req, res) => {
-    res.type('text/html');
     res.status(404);
-    res.send('404 - Not found');
+    res.render('404');
 });
 
 app.use((err, req, res, next) => {
     console.log(err.stack);
-    res.type('text/html');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), () => {
     console.log('Express started from localhost: ' + app.get('port'));
 });
-
-// page 48
